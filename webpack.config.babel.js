67547ea45409dcaa176020a920eb4bd05908b642
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2019-11-11 13:45:57 -0500

    Ajuste de webpack y realizar maquetación del landing page teniendo en cuenta la semántica HTML 5

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,7 +3,8 @@ import fs from 'fs';
 import {
   // optimize,
   DefinePlugin,
-  LoaderOptionsPlugin
+  LoaderOptionsPlugin,
+  ProvidePlugin
 } from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import CompressionPlugin from 'compression-webpack-plugin';
@@ -11,13 +12,17 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
 import ScriptExtHtmlWebpackPlugin from 'script-ext-html-webpack-plugin';
 
+// dart sass
+// import sass from 'sass';
+// import fibers from 'fibers';
+
 const expression = /^(development|production)$/;
 const nodeEnv = process.env.NODE_ENV || 'development';
 const nodeEnvValue = expression.test(nodeEnv) ? nodeEnv : 'development';
 const isDevelopment = nodeEnvValue.toLowerCase() === 'development';
 const isProduction = nodeEnvValue.toLowerCase() === 'production';
 
-const dir = (src) => (path.resolve(__dirname, src));
+const dir = src => path.resolve(__dirname, src);
 const paths = {
   src: dir('./src'),
   build: dir('./build'),
@@ -27,11 +32,12 @@ const paths = {
 // resolve
 const resolve = {
   alias: {
-    '@fonts': path.resolve(paths.assets, '/fonts'),
-    '@images': path.resolve(paths.assets, '/images'),
-    '@styles': path.resolve(paths.src, '/styles')
+    '@': paths.src,
+    '@fonts': path.resolve(paths.assets, 'fonts'),
+    '@images': path.resolve(paths.assets, 'images'),
+    '@styles': path.resolve(paths.src, 'styles')
   },
-  extensions: ['*', '.js', '.json']
+  extensions: [ '.js', '.jsx' ]
 };
 
 const babelrcFile = JSON.parse(
@@ -45,20 +51,36 @@ const rules = [
   {
     test: /\.(woff|woff2|eot|ttf|svg)(\?.*$|$)/,
     loader: 'file-loader?name=[name].[ext]?[hash]',
+    // options: {
+    //   name: '[name].[ext]?[hash]',
+    //   outputPath: 'assets'
+    // },
     include: [
-      path.resolve(paths.assets, '/fonts')
+      path.resolve(paths.assets, 'fonts')
     ]
   },
   {
-    test: /\.css$/i,
+    test: /\.(sa|sc|c)ss$/i,
     use: [
       {
         loader: MiniCssExtractPlugin.loader,
         options: {
+          // publicPath: '../',
           hmr: isDevelopment
         }
       },
-      'css-loader'
+      'css-loader',
+      'sass-loader'
+      // -fibers for dart sass
+      // {
+      //   loader: 'sass-loader',
+      //   options: {
+      //     implementation: sass,
+      //     sassOptions: {
+      //       fiber: fibers
+      //     }
+      //   }
+      // }
     ]
   },
   {
@@ -71,10 +93,11 @@ const rules = [
   },
   {
     test: /\.ico|png|gif|jpg|svg$/i,
-    loader: 'file-loader',
-    options: {
-      name: '[name].[ext]?[hash]'
-    },
+    loader: 'file-loader?name=[name].[ext]?[hash]',
+    // options: {
+    //   name: '[name].[ext]?[hash]',
+    //   outputPath: 'assets'
+    // },
     exclude: /^node_modules$/
   },
   {
@@ -91,12 +114,19 @@ const rules = [
 // plugins
 const plugins = [ /* defaults */
   new MiniCssExtractPlugin({
-    filename: isDevelopment ? '[name].css' : '[name].[hash].css',
-    chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+    filename: isDevelopment ? 'styles/[name].css' : 'styles/[name].[hash].css',
+    chunkFilename: isDevelopment ? 'styles/[id].css' : 'styles/[id].[hash].css'
   }),
   new CompressionPlugin(),
+  new ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  }),
   new HtmlWebpackPlugin({
-    template: path.resolve(paths.src, 'index.html')
+    template: path.resolve(paths.src, 'index.html'),
+    favicon: path.resolve(paths.src, 'favicon.ico'),
+    hash: isProduction
   }),
   new ScriptExtHtmlWebpackPlugin({
     defaultAttribute: 'defer'
@@ -117,21 +147,23 @@ if (isProduction) {
 const devServer = {
   contentBase: paths.src,
   historyApiFallback: true,
+  watchContentBase: true,
   noInfo: false,
   overlay: true,
-  port: 12500
-  // publicPath: path.resolve(paths.src, 'assets')
+  port: 12500,
+  compress: true
 };
 
 const config = {
   entry: [
     path.resolve(paths.src, 'main.js'),
-    path.resolve(paths.src, 'styles/_main.css')
+    path.resolve(paths.src, 'styles/_main.scss')
   ],
   output: {
     path: paths.build,
-    publicPath: '/build',
-    filename: 'bundle.js'
+    publicPath: '/',
+    filename: 'scripts/[name].js',
+    chunkFilename: 'scripts/[name].js'
   },
   module: {
     rules
